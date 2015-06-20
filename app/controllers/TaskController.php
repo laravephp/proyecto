cbce2d56aa4ecc030a92ff53bdfe13e961439d1c
ivan <sergio.ivan.galindo.diaz@gmail.com>
2015-06-19 21:13:01 -0500

    se realizo correcciones y se hizo la funcion searchTask

--- app/controllers/TaskController.php
+++ app/controllers/TaskController.php
@@ -3,10 +3,53 @@
 class TaskController extends BaseController {
 
   public function asignTask(){
-    $data = Input::only(['folio','oficioReferencia','descripcion']);
+//  $data = Input::only(['folio','oficioReferencia','descripcion']);
+  	$data =Input::all();
 
-    $task = task::create($data);
+  	 $rules = array(
+        'folio'             => 'numeric|required|unique:tasks',
+        'oficioReferencia'  => 'required',
+        'descripcion'       => 'string|required'
+        
+    );
+  
+    $messages = array(
+      'folio.unique'                => 'Folio duplicado.',
+      'folio.required'              => 'El folio es obligatorio.',
+      'oficioReferencia.required'   => 'El Oficio Referencia es obligatorio.',
+      'descripcion.required'        => 'El asunto es obligatorio.'
+      
+    );
+   
+    $validation = Validator::make($data, $rules, $messages);
+   
+    if ($validation->fails())
+    {
+      
+        return Redirect::to('dash')->withErrors($validation);
+ 
+    }else{
 
-      return View::make('auth/dash');
+         $task = task::create($data);
+
+         return View::make('auth/dash');
+    }
+
+    public function searchTask(){
+    	$search = Input::get('search');
+    $searchTerms = explode(' ', $search);
+    $query = DB::table('tasks');
+    foreach($searchTerms as $term)
+    {
+        $query->where('folio', 'LIKE', '%'. $term .'%');
+        $query->orwhere('oficioReferencia', 'LIKE', '%'. $term .'%');
+        $query->orwhere('descripcion', 'LIKE', '%'. $term .'%');
+        
+    }
+    $results = $query->get();
+    return Response::json(array(
+          'busqueda' =>  $results
+        ));
+
   }
 }
